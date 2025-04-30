02fafdc03b26e7cf5938ea661a546b44cb8832b5
dansonnn <159047052+dansonnn@users.noreply.github.com>
2025-04-30 23:20:11 +0800

    Update camel.py

--- Problem Set 2/camel.py
+++ Problem Set 2/camel.py
@@ -1,1 +1,10 @@
-test
+def main():
+    camel = input("camelCase: ")
+    print("snake_case: ", end="")
+    for i in camel:
+        if i.isupper():
+            print(i.replace(i, f'_{i.lower()}'), end="")
+        else:
+            print(i, end="")
+    print("")
+main()
